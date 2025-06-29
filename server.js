a53d72e33ceeed3db73352ac5411dc1d6ca74b6e
Vishal Kumar <bt23mme087@students.vnit.ac.in>
2025-06-30 04:52:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Catch-all handler: for any request that doesn't match a file, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
