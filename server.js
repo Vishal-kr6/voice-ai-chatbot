a449904e627e390371b6cae6753cd32ff63e7e0b
Vishal Kumar <bt23mme087@students.vnit.ac.in>
2025-06-30 04:26:37 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
